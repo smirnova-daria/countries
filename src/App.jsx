44686a0bdd2dbdbff2b2aeca2c7fd1288a794b70
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-23 18:47:12 +0300

    add list of countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ALL_COUNTRIES } from "./api/config";
+import { List } from "./components/List";
+import { Card } from "./components/Card";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  console.log(countries);
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+  }, []);
+
   return (
     <>
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.svg,
+              name: c.name,
+              info: [
+                {
+                  title: "Population",
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: "Region",
+                  description: c.region,
+                },
+                {
+                  title: "Capital",
+                  description: c.capital,
+                },
+              ],
+            };
+            return <Card key={c.name} {...countryInfo} />;
+          })}
+        </List>
       </Main>
     </>
   );
