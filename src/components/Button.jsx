bf048f31fa37ee6190b15746020f206a5330f0d1
Daria Smirnova <darya.smirn1@yandex.ru>
2022-08-23 20:52:41 +0300

    add info about country

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+export const Button = styled.button`
+  padding: 0 1rem;
+  background-color: var(--color-ui-base);
+  box-shadow: var(--shadow);
+  line-height: 2.5;
+  border-radius: var(--b-radius);
+	border: none;
+	display: flex;
+	align-items: center;
+	gap: 0.75rem;
+	color: var(--text-color);
+	cursor: pointer;
+`;
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BiArrowBack } from "react-icons/bi";
+import { useEffect, useState } from "react";
+import { searchByCountry } from "../api/config";
+import { Button } from "../components/Button";
+import { Info } from "../components/Info";
 
 export const Details = () => {
-  const params = useParams();
-  return <div>Details {params.name}</div>;
+  const { name } = useParams();
+  const navigate = useNavigate();
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        <BiArrowBack /> Back
+      </Button>
+      {country && <Info {...country} navigate={navigate} />}
+    </div>
+  );
 };
